dd8df277262d03b6a6aa84fc554a3aaba85a93e4
Maria Granados <mariafgrana2@gmail.com>
2024-10-15 09:16:51 -0600

    fix: html, css, and js code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,35 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+// 1. Renombrar las variables para brindar más claridad: con un nombre descriptivo y uso de camelCase
+// 2. Se corrigen y validan uso de selectores
+const refName = document.querySelector('.name'); // Se añadio el selector de clase "."
+const refBlog = document.querySelector('.blog'); // Se cambio el selector de id por el de clase "."
+const refLocation = document.querySelector('.location'); // Se agrego elemento y clase en html
+
+
+// 3. Se actualizan el nombre de las variables que previamenet fueron modificadas
+// 4. Se agrega la palabra reservada async  para que la funcións sea asincrona y podamos usar el operador await:
+// 5. Aplicar sintaxis de  try y catch
+async function displayUser(username) {
+  try {
+    refName.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); // Agregamos una variable para convertir la respuesta en JSON
+    console.log(data);
+
+    refName.textContent = data.name;
+    refBlog.textContent = data.blog;
+    refLocation.textContent = data.location;
+  } catch (err) { //
+    handleError(err)
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  refName.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
